98aac76e13af29eac45444635614a4cecd738a26
cahogan <cahogan16@gmail.com>
2024-04-21 14:19:15 -0700

    Add basic game page

--- core/views/game.py
+++ core/views/game.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+import core.models as models
 
 def game(request):
-    return render(request, "core/game.html")
+    game = models.Game.objects.get(id=1) # for now
+    questions = models.Question.objects.filter(game=game).order_by("order")
+    context = {
+        "game": game,
+        "questions": questions,
+    }
+    return render(request, "core/game.html", context)
+
 
