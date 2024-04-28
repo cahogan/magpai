c6bdc7f7cc1be2d61fa8bf1f9c00701194d7c698
cahogan <cahogan16@gmail.com>
2024-04-28 09:31:02 -0700

    Add some additional models for the next version of the game

--- core/models/game/models.py
+++ core/models/game/models.py
@@ -9,7 +9,26 @@ class Game(models.Model):
     id = models.AutoField(primary_key=True)
     
     # Attributes
+    intro = models.TextField(blank=True, null=True)
     name = models.CharField(max_length=100)
+    prize = models.CharField(max_length=100)
+    outro = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.name} (#{self.id})"
+
+
+class Judge(models.Model):
+    """
+    Represents a judge in a game.
+    """
+
+    id = models.AutoField(primary_key=True)
+    
+    # Attributes
+    name = models.CharField(max_length=100)
+    personality_string = models.TextField()
+    profile_image = models.ImageField(blank=True, null=True)
+    
     def __str__(self):
         return f"{self.name} (#{self.id})"
--- core/models/question/models.py
+++ core/models/question/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Question(models.Model):
@@ -20,3 +21,24 @@ class Question(models.Model):
 
     def __str__(self):
         return f"{self.name} (#{self.id})"
+
+
+class QuestionResponse(models.Model):
+    """
+    Represents a single response to a question in a game.
+    """
+
+    id = models.AutoField(primary_key=True)
+
+    # Relationships
+    question = models.ForeignKey("Question", on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    # Attributes
+    image = models.ImageField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_correct = models.BooleanField()
+
+    def __str__(self):
+        return f"Response to {self.question} by {self.user}"
+
