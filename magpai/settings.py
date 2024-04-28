f8426cfb6f9a102e2a564b59567800a3cc1acb4b
cahogan <cahogan16@gmail.com>
2024-04-28 00:12:28 -0700

    Add nginx Dockerfile; update settings to use postgres

--- magpai/settings.py
+++ magpai/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'magpai.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "test"),
+        "USER": os.getenv("POSTGRES_USER", "test"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "test"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", 5432),
+    },
 }
 
 
